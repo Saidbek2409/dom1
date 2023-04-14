aeded95abe64254c2132de8dd06e1203eae1842b
Saidbek <106152417+Saidbek2409@users.noreply.github.com>
2023-04-14 22:57:51 +0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,12 @@
+let doc = document
+let body = doc.body // обрашение у бали 
+let html = doc.documentElement 
+let h_cal =  body.children
+
+
+console.log(h_cal);
+console.log(html);
+let header = body.firstElementChild 
+console.log(header);
+
+let div1 = doc.querySelector('.header')
